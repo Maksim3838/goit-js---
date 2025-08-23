6919c5cd087e4fad7812cd9063bcd3669c1e4c12
Maksim <70702008m@gmail.com>
2025-08-23 20:54:03 +0300

    text

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,28 @@
-function isEnoughCapacity(products, containerSize){
-      const quan = Object.values(products);  
-     const tot = quan.reduce((total, quan) => total + quan, 0);
-     return tot <= containerSize;
-} 
- 
-console.log(
-  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-);
+class Car {
+
+constructor(brend, tap, cent) {
+this.brend = brend;
+this.tap = tap;
+this.cent = cent;
+}
+getcent() {
+  return this.cent;
+}
+tytyty (newcent) {
+this.cent = newcent;
+}
+
+}
+
+
+
+
+const audi = new Car (`audi`, `retro`, 1700);
+const bmw = new Car (`bmw`, `lala`, 2577);
+
+audi.tytyty(500)
+console.log(`audi`, audi.cent);
+bmw.tytyty(6666666666666);
+console.log(`bmw`, bmw.cent);
+console.log(Car);
+
